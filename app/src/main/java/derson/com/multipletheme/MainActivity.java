ddfd007b7e15871ca32e22dd4cf2ce3c76e7bc16
dersoncheng <273168148@qq.com>
2015-07-29 18:01:06 +0800

    增加了切换夜间模式时的渐变式属性动画
    增加了切换夜间模式时的渐变式属性动画，让切换过程更顺畅

--- app/src/main/java/derson/com/multipletheme/MainActivity.java
+++ app/src/main/java/derson/com/multipletheme/MainActivity.java
@@ -1,11 +1,16 @@
 package derson.com.multipletheme;
 
+import android.animation.Animator;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 
 import derson.com.multipletheme.colorUi.util.ColorUiUtil;
 import derson.com.multipletheme.colorUi.util.SharedPreferencesMgr;
@@ -31,8 +36,43 @@ public class MainActivity extends BaseActivity {
                     SharedPreferencesMgr.setInt("theme", 1);
                     setTheme(R.style.theme_2);
                 }
-                View rootView = getWindow().getDecorView();
-                ColorUiUtil.changeTheme(rootView, getTheme());
+                final View rootView = getWindow().getDecorView();
+                if(Build.VERSION.SDK_INT >= 14) {
+                    rootView.setDrawingCacheEnabled(true);
+                    rootView.buildDrawingCache(true);
+                    final Bitmap localBitmap = Bitmap.createBitmap(rootView.getDrawingCache());
+                    rootView.setDrawingCacheEnabled(false);
+                    if (null != localBitmap && rootView instanceof ViewGroup) {
+                        final View localView2 = new View(getApplicationContext());
+                        localView2.setBackgroundDrawable(new BitmapDrawable(getResources(), localBitmap));
+                        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+                        ((ViewGroup) rootView).addView(localView2, params);
+                        localView2.animate().alpha(0).setDuration(400).setListener(new Animator.AnimatorListener() {
+                            @Override
+                            public void onAnimationStart(Animator animation) {
+                                ColorUiUtil.changeTheme(rootView, getTheme());
+                            }
+
+                            @Override
+                            public void onAnimationEnd(Animator animation) {
+                                ((ViewGroup) rootView).removeView(localView2);
+                                localBitmap.recycle();
+                            }
+
+                            @Override
+                            public void onAnimationCancel(Animator animation) {
+
+                            }
+
+                            @Override
+                            public void onAnimationRepeat(Animator animation) {
+
+                            }
+                        }).start();
+                    }
+                } else {
+                    ColorUiUtil.changeTheme(rootView, getTheme());
+                }
             }
         });
         btn_next = (ColorButton)findViewById(R.id.btn_2);
